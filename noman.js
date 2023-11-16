a84fb67589ca2e478fd8a0a0c07049bb5d9d84fd
Malik Noman khan <120743420+Noman-Khan0782@users.noreply.github.com>
2023-11-16 09:23:02 +0500

    first commit

--- noman.js
+++ noman.js
@@ -7,3 +7,9 @@ console.log("Noman khan")
 console.log("Noman khan")
 console.log("Noman khan")
 console.log("Noman khan")
+console.log("------------------------------")
+'{"name":"John", "age":30, "car":null}'
+let personName = obj.name;
+let personAge = obj.age;
+console.log(personName)
+console.log(personAge)
